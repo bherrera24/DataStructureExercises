02eb928dc4abab5a04aea527b63acf857184f5bf
“Berenice <berenice.herrera@ipsos.com>
2025-08-21 13:46:06 -0400

    feat: Two sum ex

--- Arrays/TwoSum.js
+++ Arrays/TwoSum.js
@@ -1,1 +1,28 @@
-// hi
+//big O(n)
+/*
+  Cuándo usar un Map:
+
+  Se usa un Map cuando necesitas buscar elementos rápidamente mientras recorres un array.
+
+  Operación de búsqueda: O(1) en promedio.
+
+  Muy útil cuando quieres evitar bucles anidados (O(n²)).
+
+ Patrones típicos:
+
+ Encontrar pares que sumen un objetivo (Two Sum)
+
+ Contar ocurrencias de elementos
+
+ Encontrar el primer duplicado
+
+ Asociar valores con índices u otras propiedades
+*/ 
+const twoSum= (nums, target)=>{
+     const map = new Map();
+    for(let i=0; i<nums.length; i++){
+        const complement= target- nums[i]
+        if(map.get(complement)) return [map.get(complement, i)]
+        map.set(nums[i], i)
+    }
+}
