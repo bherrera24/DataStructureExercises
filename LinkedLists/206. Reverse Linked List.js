68bbca922e6f80393aa706614db5f327bbd41d85
“Berenice <bherreran24@gmail.com>
2025-11-09 21:09:10 -0300

    feat: linkeklist exercises

---
+++ LinkedLists/206. Reverse Linked List.js
@@ -0,0 +1,11 @@
+const ReverseLinkList= (head)=>{
+   let prev=null
+   let curr=head
+    while(curr!==null){
+        const nextTemp=curr.next // 1) guarda el siguiente
+        curr.next=prev  //2) invierte el enlace
+        prev.next=nextTemp //3) avanza el prev
+
+    }
+    return prev //devuelve el nuevo head
+}
